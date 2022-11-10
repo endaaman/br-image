4e8a706f63cb75911d6a8f593317e6f9d378b463
endaaman <endaaman@frontier.hokudai.ac.jp>
2022-11-10 09:09:26 +0900

    split

--- pre.py
+++ pre.py
@@ -34,3 +34,22 @@ class C(Trainer):
                 t = 'hor_X'
             ds.df.at[i.id, 'image_type'] = t
         ds.df.to_csv('l.csv')
+
+    def arg_split(self, parser):
+        parser.add_argument('--ratio', '-r', type=float, default=0.3)
+
+    def run_split(self):
+        # cache label data
+        df = pd.read_excel('data/master.xlsx', index_col=0).dropna()
+        df_train, df_test = train_test_split(df, test_size=self.args.ratio, stratify=df.diagnosis)
+        df['test'] = 0
+        df.at[df_test.index, 'test'] = 1
+        os.makedirs('data/cache', exist_ok=True)
+        p = 'data/cache/labels.csv'
+        df.to_csv(p)
+
+        # cache images
+        for f in glob('data/images/*.png'):
+            basename = os.path.basename(f)
+
+        print(f'wrote {p}')
