c742ec4fae9a58c5575a223c200083cb9d548419
endaaman <endaaman@frontier.hokudai.ac.jp>
2022-12-01 16:35:08 +0900

    fix rule def

--- batch.py
+++ batch.py
@@ -17,7 +17,6 @@ import pydicom
 from endaaman import Commander, with_wrote, with_log
 
 
-
 class ROI(NamedTuple):
     x: int
     y: int
@@ -27,59 +26,70 @@ class ROI(NamedTuple):
         return (self.x, self.y, self.x + self.w, self.y + self.h)
 
 class ROIRule(NamedTuple):
-    enhance_roi: ROI
-    plain_roi: ROI
+    e_xy: tuple[int, int]
+    p_xy: tuple[int, int]
+    size: tuple[int, int]
+
+    def e_rect(self):
+        return (self.e_xy[0], self.e_xy[1], self.e_xy[0]+self.size[0], self.e_xy[1]+self.size[1])
+
+    def p_rect(self):
+        return (self.p_xy[0], self.p_xy[1], self.p_xy[0]+self.size[0], self.p_xy[1]+self.size[1])
 
 roi_rules = {
     # VERTICAL
     'C_ver': ROIRule( # (1280, 960)
-        # ROI(345, 169, 591, 293), # [1] h:169
-        # ROI(345, 588, 591, 293),
-
-        ROI(345, 142, 590, 347),
-        ROI(345, 560, 590, 347),
-    ),
-
-    'C_ver2': ROIRule( # (1280, 960)
-        ROI(256, 140, 796, 344),
-        ROI(256, 560, 796, 344),
+        # (256, 140),
+        # (256, 560),
+        # (796, 344),
+        (345, 142),
+        (345, 560),
+        (590, 347)
     ),
 
     # HORIZONTAL
     'A_hor': ROIRule( # (960, 720)
-        ROI(62, 87, 417, 495),
-        ROI(479, 87, 417, 495),
+        (62,  87),
+        (479, 87),
+        (417, 495),
     ),
 
     'B_hor': ROIRule( # (1280, 720)
-        ROI(18, 102, 554, 435),
-        ROI(605, 102, 554, 435),
+        (18,  102),
+        (605, 102),
+        (554, 435)
     ),
 
     'C_hor': ROIRule( # (1280, 960)
-        ROI(116, 144, 520, 658),
-        ROI(640, 144, 520, 658),
+        (116, 144),
+        (640, 144),
+        (520, 658)
     ),
 
     'C_hor2': ROIRule( # (1280, 960)
-        ROI(40, 141, 560, 680),
-        ROI(680, 141, 560, 680),
+        (40, 141),
+        (680, 141),
+        (560, 680),
     ),
 
     'D_hor': ROIRule( # (1552, 873)
-        ROI(74, 109, 650, 570),
-        ROI(724, 109, 650, 570),
+        (74, 109),
+        (724, 109),
+        (650, 570),
     ),
 
     'D_hor2': ROIRule( # (1552, 873)
-        ROI(2, 133, 720, 636),
-        ROI(724, 133, 720, 636),
+        (2, 133),
+        (724, 133),
+        (720, 636),
     ),
 
     'D_hor3': ROIRule( # (1552, 873)
-        ROI(45, 129, 640, 640),
-        ROI(766, 129, 640, 640),
+        (45, 129),
+        (766, 129),
+        (640, 640),
     ),
+
 }
 
 def get_default_style_by_image(img):
@@ -172,7 +182,8 @@ class C(Commander):
         parser.add_argument('--dest', default='data/cache/')
         parser.add_argument('--target', '-t', type=str, nargs='+', default=[])
         parser.add_argument('--swap', action='store_true')
-        parser.add_argument('--range', type=int, nargs='+')
+        parser.add_argument('--from', type=int)
+        parser.add_argument('--to', type=int)
 
     def run_cache(self):
         df = pd.read_excel('data/label.xlsx', index_col=0).dropna()
@@ -183,14 +194,22 @@ class C(Commander):
 
         total = len(df)
         for i, (idx, row) in (t:=tqdm(enumerate(df.iterrows()), total=total)):
-            rule = roi_rules.get(f'{row.resolution}_{row.position}', None)
+            ii = int(re.match(r'.*(\d\d\d)_\d$', idx)[1])
+            _from = getattr(self.args, 'from')
+            if _from and ii < _from:
+                continue
+            _to = getattr(self.args, 'to')
+            if _to and ii > _to:
+                continue
+            rule_code = f'{row.resolution}_{row.position}'
+            rule = roi_rules.get(rule_code, None)
             if not rule:
-                raise ValueError(f'{p} is unknown image type.')
+                raise ValueError(f'{idx}({rule_code}) is unknown image type.')
 
             original_image = Image.open(f'data/images/{idx}.png')
 
-            plain_image = original_image.crop(rule.plain_roi.rect()).convert('L')
-            enhance_image = original_image.crop(rule.enhance_roi.rect()).convert('L')
+            plain_image = original_image.crop(rule.p_rect()).convert('L')
+            enhance_image = original_image.crop(rule.e_rect()).convert('L')
             assert enhance_image.size == plain_image.size
             base_size = plain_image.size
             if row['swap'] ^ self.args.swap:
