bd87a2347f554ef646433d22418f312cf4a3737e
endaaman <ken@endaaman.me>
2022-12-08 10:03:24 +0900

    update

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ from glob import glob
 
 import numpy as np
 import torch
+from sklearn import metrics
 from torch import nn
 from tqdm import tqdm
 from torchvision import transforms
@@ -11,7 +12,6 @@ import torchvision.transforms.functional as F
 from torchvision.utils import make_grid, save_image
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
-from sklearn import metrics
 
 # from gradcam.utils import visualize_cam
 # from gradcam import GradCAM, GradCAMpp
@@ -73,13 +73,39 @@ class CMD(TorchCommander):
                 'test': int(item.test),
                 'gt': item.diagnosis,
                 'pred': pred,
-                'thres0.5': int(pred > 0.5),
-                'correct0.5': int((pred > 0.5) == item.diagnosis),
-                'thres0.3': int(pred > 0.3),
-                'correct0.3': int((pred > 0.3) == item.diagnosis),
             })
+
         df = pd.DataFrame(oo)
-        df.to_excel(with_wrote(f'out/{self.checkpoint.full_name()}/report_{self.args.target}.xlsx'), index=False)
+        mm = {}
+
+
+        fpr, tpr, thresholds = metrics.roc_curve(df.gt, df.pred)
+
+        scoress = {
+            # f1: point to maximize f1 score
+            'f1': [metrics.f1_score(df.gt, pred > t) for t in thresholds],
+
+            # youden: tanget to 45degree line
+            'youden': tpr - fpr,
+
+            # top-left: nearest to top-left corner
+            'top-left': (- tpr + 1) ** 2 + fpr ** 2,
+        }
+
+        for name, scores in scoress.items():
+            idx = np.argmax(scores)
+            mm[name] = {
+                'threshold': thresholds[idx],
+                'recall': tpr[idx],
+                'spec': 1 - fpr[idx],
+            }
+
+        df2 = pd.DataFrame(mm)
+
+        dest_path = f'out/{self.checkpoint.full_name()}/report_{self.args.target}.xlsx'
+        with pd.ExcelWriter(dest_path) as w: # pylint: disable=abstract-class-instantiated
+            df.to_excel(w, sheet_name='values', index=False)
+            df2.to_excel(w, sheet_name='metrics', index=False)
 
     def load_images_from_dir_or_file(self, src):
         paths = []
